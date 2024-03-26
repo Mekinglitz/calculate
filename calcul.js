b4a1332418f6ccdd9356b8840227cdbd39c91e58
Mekinglitz <ekeminiokon78@gmail.com>
2024-03-26 09:24:07 +0100

    first commit

---
+++ calcul.js
@@ -0,0 +1,29 @@
+
+
+/* function multiply(num1, num2){
+    const multiply = num1 * num2
+
+    return multiply
+}
+
+console.log(multiply(2,5)) */
+
+/* function divide(num1, num2){
+    const divide = num1 / num2
+
+    return divide
+}
+
+console.log(divide(500,5)) */
+/* console.log(divide(200,5))
+console.log(divide(600,5))
+console.log(divide(800,5))
+console.log(divide(300,5)) */
+
+function subtract(num1, num2){
+    const subtract = num1 - num2
+
+    return subtract
+}
+
+console.log(subtract(500,5))
